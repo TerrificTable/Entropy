b39814a2c9bb7e40d8e809b2ec84863f50114690
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-09-22 19:13:36 +0200

    simple dll code thing

---
+++ C++/dll-inject/test-dll/test-dll.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+
+// g++ compile DLL:
+// g++ -shared -o <out>.dll <src>.cpp
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+            MessageBoxA(0, "Hello World!", "Hello World", 0);
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+
+    return TRUE;
+}
